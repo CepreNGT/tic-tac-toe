7d33e921f8e13c73aed6d80462da73d6504c5ca3
CepreNGT <malitevs@gmail.com>
2025-02-25 17:24:17 +0500

    Added colored winner

--- index.js
+++ index.js
@@ -77,54 +77,63 @@ function resetClickHandler () {
     console.log('reset!');
 }
 
+function colorCell (row, col, color = 'red') {
+    const targetCell = findCell(row, col);
+    targetCell.style.color = color;
+}
+
 function checkFieldForWinner(row, col) {
     let rows = [row - 2, row - 1, row, row + 1, row + 2];
     let cols = [col - 2, col - 1, col, col + 1, col + 2];
     let symbol = 0;
-    count = 0;
+    let goodCells = [];
     for (let i = 0; i < 5; i++) {
         if (rows[i] < 0 || rows[i] > dimension - 1) continue;
         if (symbol === 0) symbol = field[rows[i]][col];
-        if (field[rows[i]][col] === symbol) count++;
-        else count = 0;
+        if (field[rows[i]][col] === symbol) goodCells.push([rows[i], col]);
+        else goodCells = [];
     }
-    if (count >= 3 && symbol !== EMPTY) {
+    if (goodCells.length === 3 && symbol !== EMPTY) {
+        goodCells.forEach(cell => colorCell(cell[0], cell[1]));
         alert(`Победил ${symbol}!`);
         return;
     }
     symbol = 0;
-    count = 0;
+    goodCells = [];
     for (let i = 0; i < 5; i++) {
         if (cols[i] < 0 || cols[i] > dimension - 1) continue;
         if (symbol === 0) symbol = field[row][cols[i]];
-        if (field[row][cols[i]] === symbol) count++;
-        else count = 0;
+        if (field[row][cols[i]] === symbol) goodCells.push([row, cols[i]]);
+        else goodCells = [];
     }
-    if (count >= 3 && symbol !== EMPTY) {
+    if (goodCells.length === 3 && symbol !== EMPTY) {
+        goodCells.forEach(cell => colorCell(cell[0], cell[1]));
         alert(`Победил ${symbol}!`);
         return;
     }
     symbol = 0;
-    count = 0;
+    goodCells = [];
     for (let i = 0; i < 5; i++) {
         if (cols[i] < 0 || cols[i] > dimension - 1 || rows[i] < 0 || rows[i] > dimension - 1) continue;
         if (symbol === 0) symbol = field[rows[i]][cols[i]];
-        if (field[rows[i]][cols[i]] === symbol) count++;
-        else count = 0;
+        if (field[rows[i]][cols[i]] === symbol) goodCells.push([rows[i], cols[i]]);
+        else goodCells = [];
     }
-    if (count >= 3 && symbol !== EMPTY) {
+    if (goodCells.length === 3 && symbol !== EMPTY) {
+        goodCells.forEach(cell => colorCell(cell[0], cell[1]));
         alert(`Победил ${symbol}!`);
         return;
     }
     symbol = 0;
-    count = 0;
+    goodCells = [];
     for (let i = 0; i < 5; i++) {
         if (cols[i] < 0 || cols[i] > dimension - 1 || rows[4 - i] < 0 || rows[4 - i] > dimension - 1) continue;
         if (symbol === 0) symbol = field[rows[4 - i]][cols[i]];
-        if (field[rows[4 - i]][cols[i]] === symbol) count++;
-        else count = 0;
+        if (field[rows[4 - i]][cols[i]] === symbol) goodCells.push([rows[4 - i], cols[i]]);
+        else goodCells = [];
     }
-    if (count >= 3 && symbol !== EMPTY) {
+    if (goodCells.length === 3 && symbol !== EMPTY) {
+        goodCells.forEach(cell => colorCell(cell[0], cell[1]));
         alert(`Победил ${symbol}!`);
         return;
     }
